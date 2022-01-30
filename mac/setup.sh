3691d68bcbd245f958791850c6fe8f7e827b5764
Christian Wolfram <c.wolfram@chriwo.de>
2022-01-30 19:02:45 +0100

    Update setup.sh

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-# Install apps and binaries with Brew
-source mac/brew-essentials.sh
+# Install apps with Brew
+source ./brew-essentials.sh
+
+# Install binaries with Brew
+source ./brew-binaries.sh
 
 # Configure MacOS defaults.
 # You only want to run this once during setup. Additional runs may reset changes you make manually.
